07ede7a760bdfcb4ebafe61bd6aebcb756928d62
mktcl <162700341+mktcl@users.noreply.github.com>
2024-09-25 17:28:36 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 
   const divText = document.querySelector('div').innerText;
-  console.log(`${divText}`, 'font-size:3rem;color:orangered;');
+  console.log(`%c${divText}`, 'font-size:3rem;color:orangered;');
