b5211b25c6526ef52f12391374673606a44a216a
mktcl <162700341+mktcl@users.noreply.github.com>
2024-09-25 17:57:15 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-const divText = document.querySelector('div').innerText;
+'use strict'
+!function(){
+  const divText = document.querySelector('div').innerText;
 console.log('%cinjectect from github', 'font-size:3rem;color:orangered;');
 console.log(`%c${divText}`, 'font-size:3rem;color:orangered;');
+}();
 
 
