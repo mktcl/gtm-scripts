01d37714f583d9350d5c8805a4d47194bb90bc42
mktcl <162700341+mktcl@users.noreply.github.com>
2024-09-26 14:33:16 +0200

    Create form-change.js

---
+++ form-change.js
@@ -0,0 +1,17 @@
+(function(){
+var targetsName = new RegExp(/(?:email|dateOfBirth|phoneNumber|fullname|houseNumber|postcode|house|street|town|county|username|password|depositAmount|limitAmount|fullName|(first|last)\sname)/);
+  
+var pushToDataLayer = function(el){          
+  return dataLayer.push({event:'form_change', input_name: el.target.getAttribute('name'), input_label:(el.target.previousElementSibling && el.target.previousElementSibling.innerText)})
+
+};
+
+  
+var targetElements = Array.from(document.querySelectorAll('input')).filter(function(el){return targetsName.test(el.getAttribute('name'))});
+
+// Clear any listeners leftover from navigation  
+targetElements.forEach(function(el){return el.removeEventListener('change', pushToDataLayer)});   
+
+// Add listener  
+targetElements.forEach(function(el){return el.addEventListener('change', pushToDataLayer)});   
+})();
