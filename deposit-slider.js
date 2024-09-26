c8ff9735949d49e427b0c1ee6e9d2d88fecd26b3
mktcl <162700341+mktcl@users.noreply.github.com>
2024-09-26 14:21:46 +0200

    Create deposit-slider.js

---
+++ deposit-slider.js
@@ -0,0 +1,12 @@
+(function(){  
+  var target = document.querySelector('input[type="range"]');
+  var pushToDataLayer = function(){
+  dataLayer.push({event:'form_change', input_name:'deposit_slider', input_label: null})
+  }
+
+  // Clear any listeners leftover from navigation  
+  target.removeEventListener('change', pushToDataLayer);
+  
+  // Add listener  
+  target.addEventListener('change', pushToDataLayer);  
+})();  
