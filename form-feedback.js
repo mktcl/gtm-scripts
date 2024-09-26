4c4b4bd5b5368163eb8e8b9023ceab5a80df5b30
mktcl <162700341+mktcl@users.noreply.github.com>
2024-09-26 15:47:26 +0200

    Create form-feedback.js

---
+++ form-feedback.js
@@ -0,0 +1,12 @@
+(function(){  
+  console.log('%cform_feedback github script', 'font-size:2rem;color:red;');
+  let form_feedback_element = document.querySelector('[class="feedback-entry invalid-feedback-entry"]');
+  let feedback_message = formFeedbackElement.innerText;
+  let input_name = form_feedback_element.closest('div').querySelector('input') && form_feedback_element.closest('div').querySelector('input').getAttribute('name');
+  let input_name = form_feedback_element.closest('div').querySelector('label') && form_feedback_element.closest('div').querySelector('label').innerText);
+
+
+dataLayer.push({event:'form_feedback', feedback_message, input_name, input_label});
+  
+
+})()
